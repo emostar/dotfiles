6d070150627fc1869f3a3bb24185c97592d4ba24
Jon Keating <jon@licq.org>
2012-10-31 14:33:27 +0900

    Set things up

---
+++ setup.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+if [ -d ~/.my-dotfiles ]
+then
+  echo "\033[0;33mYou already have My Dotfiles installed.\033[0m You'll need to remove ~/.my-dotfiles if you want to install"
+  exit
+fi
+
+echo "\033[0;34mCloning My Dotfiles...\033[0m"
+hash git >/dev/null && /usr/bin/env git clone https://github.com/mathrawka/dotfiles.git ~/.my-dotfiles || {
+  echo "git not installed"
+  exit
+}
+
+# Install some packages
+# TODO Check if I ever use a non debian based distro
+sudo apt-get install htop
+
+# Install oh-my-zsh
+curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
+
+# Install dotvim
+git >/dev/null && /usr/bin/env git clone https://github.com/mathrawka/dotvim.git ~/.my-dotfiles/dotvim
+cd ~/.my-dotfiles/dotvim
+setup.sh
+
+# Copy the bin files
+mkdir -p ~/bin
+cp bin/* ~/bin
+
+# Copy the dotfiles
+cp dotfiles/* ~
